96beef991acd4c84d1793df4118625188cda598d
Thanabodee <wingyminus@gmail.com>
2014-07-10 22:53:54 +0700

    refactor create basic auth string to private method

--- basicauth.go
+++ basicauth.go
@@ -13,14 +13,16 @@ type BasicAuthHandler struct {
 func (bh *BasicAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	authorization := r.Header.Get("Authorization")
 
-	userAndPass := "Basic " + base64.StdEncoding.EncodeToString([]byte(bh.username+":"+bh.password))
-
-	if authorization == userAndPass {
+	if authorization == bh.basicUserAndPass() {
 		bh.f(w, r)
 	} else {
 		w.Header().Add("WWW-Authenticate", `Basic realm="My Server"`)
 		http.Error(w, "", http.StatusUnauthorized)
 	}
+}
+
+func (bh *BasicAuthHandler) basicUserAndPass() string {
+	return "Basic " + base64.StdEncoding.EncodeToString([]byte(bh.username+":"+bh.password))
 }
 
 func BasicAuthHandleFunc(f func(w http.ResponseWriter, r *http.Request), username, password string) http.Handler {
